78ad5a9eb2fc8939f22cdb41cae9aa321b7308ad
PhungDinh <SUCCESS\phungdinh@phungdinh.success.local>
2015-04-17 14:51:38 +0700

    Initial commit

---
+++ hello.js
@@ -0,0 +1,33 @@
+/**
+ * Created by SUCCESS\phungdinh on 4/17/15.
+ */
+var http = require('http'),
+    fs = require('fs');
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write("Hello World\n\n");
+
+    // length of file
+    var newfile = fs.createWriteStream('myfile.txt');
+    var fileLength = req.headers['content-length'];
+    var uploadedBytes = 0;
+
+    //req.pipe(res, { end: false });
+    //res.end();
+    req.pipe(newfile);
+    req.on('data', function(chunk){
+        uploadedBytes += chunk.length;
+        //completed percent
+        var progress = uploadedBytes/fileLength * 100;
+        // display complete percent
+        console.log('Uploaded: ' + parseInt(progress, 10) + '%');
+        res.write('Uploaded: ' + parseInt(progress, 10) + '% \n');
+    });
+
+    req.on('end', function(){
+        res.end('Upload successful!');
+    });
+
+}).listen(3000);
+
+console.log("Server listening in port 3000");
