1ea83df768301f9359b756963682c2a9b4447288
yohananjr13 <yohananjr13@gmail.com>
2015-04-20 14:28:28 +0700

    demo module

---
+++ 8-app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by SUCCESS\phungdinh on 4/20/15.
+ */
+
+var movies = require('./8-movies');
+
+movies.PaulWalker();
+//movies.printFast7();
+//movies.printCTNA();
---
+++ 8-movies.js
@@ -0,0 +1,13 @@
+/**
+ * Created by SUCCESS\phungdinh on 4/20/15.
+ */
+
+function printFast7(){
+    console.log("Fast and Furious 7: PG-14");
+}
+
+function printCTNA(){
+    console.log("CTNA: PG-100");
+}
+
+module.exports.PaulWalker = printFast7;
