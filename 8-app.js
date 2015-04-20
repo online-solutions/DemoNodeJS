6ae2d3ebb0d2e692d1fa01423d50478a32eb7ff1
yohananjr13 <yohananjr13@gmail.com>
2015-04-20 14:51:28 +0700

    demo module 2

--- 8-app.js
+++ 8-app.js
@@ -7,3 +7,7 @@ var movies = require('./8-movies');
 movies.PaulWalker();
 //movies.printFast7();
 //movies.printCTNA();
+
+var newMovies = require('./9-new-movies');
+newMovies.printAvatar();
+console.log(newMovies.favMovie);
---
+++ 9-new-movies.js
@@ -0,0 +1,15 @@
+/**
+ * Created by SUCCESS\phungdinh on 4/20/15.
+ */
+module.exports = {
+    printAvatar: function (){
+        console.log("Avatar movie");
+    },
+
+    printChappie: function (){
+        console.log("Chappie movie");
+    },
+
+    favMovie: "The Matrix"
+}
+
