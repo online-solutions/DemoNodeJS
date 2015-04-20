ab599f4de8e94cf53e21dcd6b86686f836c119b8
yohananjr13 <yohananjr13@gmail.com>
2015-04-20 13:25:57 +0700

    demo this

--- play.js
+++ play.js
@@ -4,10 +4,29 @@
 
 var lite = {
     favFood: "beefsteak",
-    favMovie: "Snowy"
+    favMovie: "Snowy",
+    printlnSomething: function(){
+        console.log("My name is Lite");
+        console.log(this === lite);
+    }
 }
 
 var key = lite;
 key.favFood = "rice";
 
 console.log(lite.favFood);
+
+// test == and ===
+
+console.log(21 == '21'); // true
+console.log(21 === '21'); // false
+
+
+// test this
+function doSomethingOutOfObject(){
+    console.log("\n I am your King");
+    console.log(this == global);
+}
+
+lite.printlnSomething();
+doSomethingOutOfObject();
